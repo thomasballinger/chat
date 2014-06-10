bca78271db56b949604bcff5b795ad3be203a446
Thomas Ballinger <thomasballinger@gmail.com>
2014-06-10 14:19:27 -0400

    added colors to confirm understanding of encodings

--- chat.py
+++ chat.py
@@ -4,13 +4,21 @@ import sys
 
 from oldchat import CharacterAtATime
 
+def green(msg):
+    sys.stdout.write('\x1b[31m\x1b[44m' + msg + '\x1b[49m\x1b[39m')
+    sys.stdout.flush()
+
+def yellow(msg):
+    sys.stdout.write('\x1b[34m\x1b[41m' + msg + '\x1b[49m\x1b[39m')
+    sys.stdout.flush()
+
 @asyncio.coroutine
 def listen_to_server(reader):
     while True:
-        line = yield from reader.read(1)
-        if not line:
+        c = yield from reader.read(1)
+        if not c:
             break
-        print('line:', line)
+        yellow(c.decode('utf8'))
 
 @asyncio.coroutine
 def connect(loop):
@@ -18,8 +26,9 @@ def connect(loop):
 
     asyncio.async(listen_to_server(server_reader))
     def on_stdin_read():
-        print('reading on stdin')
-        server_writer.write(sys.stdin.readline().encode('ascii'))
+        c = sys.stdin.read(1)
+        green(c)
+        server_writer.write(c.encode('utf8'))
     loop.add_reader(sys.stdin, on_stdin_read)
 
 loop = asyncio.get_event_loop()
